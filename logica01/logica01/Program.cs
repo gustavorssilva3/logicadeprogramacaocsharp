d38228c1e16d61d6a9d161f36ad2c4013567a696
GUSTAVO RODRIGUES SOARES DA SILVA <gustavo.rssilva3@senacsp.edu.br>
2025-01-22 15:01:15 -0300

    logica22-01

---
+++ logica01/logica01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variaveis
+            char letra = 'A';
+            string nome = "João";
+            float temperatura = 36.5f;
+            double salario = 12345.67;
+            bool estaChovendo = true;
+
+            // Alterando as variaveis
+            letra = 'B';
+             nome = "Maria";
+             temperatura = 37.0f;
+             salario = 15000.50;
+             estaChovendo = false;
+
+            // Constantes 
+            const char simbolo = '$';
+            const string saudacao = "Bem-vindo ao C#";
+            const float pi = 3.14159f;
+            const double altura = 1.75;
+            const bool eVerdade = false;
+
+            // char simbolo = '#'
+
+            Console.WriteLine("Valores originais:");
+            Console.WriteLine($"Letra: {letra}");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Temperatura: {temperatura}");
+            Console.WriteLine($"Salário: {salario}");
+            Console.WriteLine($"Está chovendo? {estaChovendo}");
+            Console.WriteLine($"Símbolo: {simbolo}");
+            Console.WriteLine($"Saudação: {saudacao}");
+            Console.WriteLine($"Pi: {pi}");
+            Console.WriteLine($"Altura: {altura}");
+            Console.WriteLine($"É verdade? {eVerdade}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
